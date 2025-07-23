6dff8bbe5fd28383231a071f33cd28a0db8cf5c3
famakupon <famapokt@gmail.com>
2025-07-23 12:32:48 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-document.getElementById("promoForm").addEventListener("submit", async function(e) {
+document.getElementById("form").addEventListener("submit", async function (e) {
   e.preventDefault();
-  const nama = document.getElementById("nama").value;
-  const telefon = document.getElementById("telefon").value;
-  const jabatan = document.getElementById("jabatan").value;
+
+  // Tunjuk loading
+  document.getElementById("loading").style.display = "block";
+  document.getElementById("result").innerHTML = "";
+
+  const data = {
+    nama: document.getElementById("nama").value,
+    telefon: document.getElementById("telefon").value,
+    jabatan: document.getElementById("jabatan").value
+  };
 
   const response = await fetch("https://script.google.com/macros/s/AKfycbzFP50C0ZORjqs3UZL1QkgyTYsDBiXTQw26ikHcxkFndKvN99DwU6-jsy74vO9ffo6zVA/exec", {
     method: "POST",
-    body: JSON.stringify({ nama, telefon, jabatan })
+    body: JSON.stringify(data)
   });
 
   const text = await response.text();
-  document.getElementById("result").innerHTML = text.replace(/\\n/g, "<br>");
 
+  // Sembunyikan loading
+  document.getElementById("loading").style.display = "none";
+
+  // Papar kod promosi
+  document.getElementById("result").innerHTML = text.replace(/\n/g, "<br>");
 });
